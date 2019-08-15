d18603d216965ac8cdac16276a1e0e8b18923a28
Kashish Madan <kashish.madan@outlook.com>
2019-08-16 01:21:06 +0530

    Add files via upload
    Program1 Added

---
+++ program1.c
@@ -0,0 +1,11 @@
+/* ##########################################
+To print Hello World without using semi colon
+########################################## */
+
+#include<stdio.h>
+int main()
+{
+    if(printf("Hello World!"))
+    return 0;
+}
+
