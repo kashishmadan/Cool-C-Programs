7b7ba28685057cd6dead28924157716684279186
Kashish Madan <kashish.madan@outlook.com>
2019-08-17 20:55:02 +0530

    Program6 added

---
+++ program6.c
@@ -0,0 +1,26 @@
+/* #########################################################
+Program to find the size of a file in Linux or Windows
+######################################################### */
+#include <stdio.h>
+
+int main()
+{
+    FILE *fp; /*to create file*/
+    long int size=0;
+
+    /*Open file in Read Mode*/
+    fp=fopen("temp.txt","r");
+
+    /*Move file point at the end of file.*/
+    fseek(fp,0,SEEK_END);
+
+    /*Get the current position of the file pointer.*/
+    size=ftell(fp);
+
+    if(size!=-1)
+        printf("File size is: %ld\n",size);
+    else
+        printf("There is some ERROR.\n");
+
+    return 0;
+}
